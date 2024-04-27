b93f2cabe0105f8bd82563dc677f93c5de68cd1d
KOSASIH <kosasihg88@gmail.com>
2024-04-28 03:10:35 +0700

    Create qsvm.py

---
+++ algorithms/qsvm.py
@@ -0,0 +1,68 @@
+# quantum_ml_library/algorithms/qsvm.py
+
+from qiskit.circuit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit.visualization import plot_histogram, plot_bloch_vector
+from qiskit.aqua.components.multiclass_extension import MulticlassExtension
+from qiskit.aqua.components.feature_map import RawFeatureMap
+from qiskit.aqua.components.variational_forms import RYRZVariationalForm, RYVariationalForm
+from qiskit.aqua.algorithms import NumPyMinimumEigensolver, QAOA, VQE
+from qiskit.aqua.utils.support import gradient_descent
+from qiskit.circuit.library import ZZFeatureMap
+
+class QSVM:
+    def __init__(self, feature_map, variational_form, num_qubits, quantum_instance=None):
+        self.feature_map = feature_map
+        self.variational_form = variational_form
+        self.num_qubits = num_qubits
+        self.quantum_instance = quantum_instance
+
+    def fit(self, x, y):
+        # Initialize the quantum circuit and classical register
+        qr = QuantumRegister(self.num_qubits)
+        cr = ClassicalRegister(self.num_qubits)
+        qc = QuantumCircuit(qr, cr)
+
+        # Apply the feature map to the quantum circuit
+        self.feature_map.construct_circuit(qr)
+
+        # Apply the variational form to the quantum circuit
+        self.variational_form.construct_circuit(qr)
+
+        # Define the objective function for the quantum circuit
+        def objective_function(x):
+            # Set the variational parameters for the quantum circuit
+            for i, param in enumerate(x):
+                self.variational_form.set_parameters([param])
+
+            # Measure the quantum circuit and return the result
+            result = qc.execute(quantum_instance=self.quantum_instance)
+            counts = result.get_counts(qc)
+            return -counts[f'1{cr[0]}']
+
+        # Optimize the quantum circuit using gradient descent
+        x = gradient_descent(objective_function, initial_point=np.zeros(self.variational_form.num_parameters))
+
+        # Set the optimized variational parameters for the quantum circuit
+        for i, param in enumerate(x):
+            self.variational_form.set_parameter(i, param)
+
+        # Train the quantum support vector machine
+        self.training_data = x
+
+    def predict(self, x):
+        # Initialize the quantum circuit and classical register
+        qr = QuantumRegister(self.num_qubits)
+        cr = ClassicalRegister(self.num_qubits)
+        qc = QuantumCircuit(qr, cr)
+
+        # Apply the feature map to the quantum circuit
+        self.feature_map.construct_circuit(qr)
+
+        # Set the variational parameters forthe quantum circuit
+        for i, param in enumerate(self.training_data):
+            self.variational_form.set_parameter(i, param)
+
+        # Measure the quantum circuit and return the result
+        result = qc.execute(quantum_instance=self.quantum_instance)
+        counts = result.get_counts(qc)
+        return np.argmax(list(counts.values()))
